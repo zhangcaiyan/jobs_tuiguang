ebbba9c573412a11f0043defff0577690f11ecd1
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-01-22 16:29:30 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobsTuiguang::Application.config.secret_key_base = 'd3254ee5257c5966910d951675a95e7a71de054f6e40e1edd08f13e7645bf049f4832a36f9cba02b84bd90056ca884756fe5308fabbb2ffea099d483676b4701'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JobsTuiguang::Application.config.session_store :cookie_store, key: '_jobs_tuiguang_session'
