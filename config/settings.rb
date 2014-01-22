001bbd2b22366de2e461e05b5c129323998d196b
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-01-22 17:35:05 +0800

    基本设置

--- config/settings.rb
+++ config/settings.rb
@@ -3,6 +3,11 @@ module Setting
     @@root = File.expand_path('../settings', __FILE__)
   end
 
+  class Setting::Config < Setting::Base
+    source "#{@@root}/config.yml"
+    namespace Rails.env
+  end
+
   class Setting::Smtp < Setting::Base
     source "#{@@root}/smtp.yml"
     namespace Rails.env
